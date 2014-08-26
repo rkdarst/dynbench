7bfdd6a702c89be484e201d36d13d4c873436a49
Richard Darst <rkd@zgib.net>
2014-08-26 15:00:13 +0300

    tests: Update distribution assertion

--- tests.py
+++ tests.py
@@ -5,23 +5,35 @@ import scipy.stats as stats
 from nose.tools import *
 
 
-def assert_distribution(f, mean, std):
+def assert_distribution(f, mean, std, p=.1):
     vals = [ ]
 
-    for x in range(10):
-        vals.append(f())
+    for N in [10, 20, 50, 100, 500]:
+        while len(vals) < N:
+            vals.append(f())
+        #print vals
 
-    smean, sstd = np.mean(vals), np.std(mean)
-    if sstd == 0:
-        sstd = 1e9
+        smean, sstd = np.mean(vals), np.std(vals)
+        if sstd == 0:
+            sstd = 1e-6
 
-    t = (x - smean) / float(sstd / math.sqrt(len(vals)))
-    print t
+        t = (mean - smean) / float(sstd / math.sqrt(len(vals)))
+        t = -abs(t)
+        print t, mean, smean, sstd
 
-    p = stats.t.cdf(t, 9)
-    assert p == abs(p)
+        p1 = stats.t.cdf(t, len(vals)-1)
+        assert p1 == abs(p1)
 
-    assert p > .5
+        p2 = 2*p1
+        print "p:", p2
+
+        if p2 > p:
+            print "passed with N=%s"%N
+            return True   # test passed
+    raise AssertionError("Test failed, (%s !> %s) with %s samples"%(p2, p, N))
+
+def test_assert_dist():
+    assert_distribution(stats.norm(0).rvs, 0, 1)
 
 
 def test():
